28f2938270e4881909461c61df34f64d7bcb4524
ROBODORES <35854495+ROBODORES@users.noreply.github.com>
2018-03-28 19:23:30 -0700

    Update etc.java

--- src/org/usfirst/frc/team4255/robot/etc.java
+++ src/org/usfirst/frc/team4255/robot/etc.java
@@ -1,6 +1,20 @@
 package org.usfirst.frc.team4255.robot;
 
 public class etc {
+	public static double[][] middleL = {
+		{2.0, 0.0},
+		{0.0, -45.0},
+		{6.25, 0.0},
+		{0, 45.0},
+	};
+	
+	public static double[][] middleR = {
+		{2.42, 0.0},
+		{0.0, 45.0},
+		{5.07, 0.0},
+		{0, -45.0},
+	};
+	
 	public static double map(double val, double fromMin, double fromMax, double toMin, double toMax) {
 		double output = ((val-fromMin)*((toMax-toMin)/(fromMax-fromMin)))+toMin;
 		return output;
