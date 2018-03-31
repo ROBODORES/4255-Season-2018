45934a7ac0a955fc86bdaae7774e67ffb9b7b60d
ROBODORES <35854495+ROBODORES@users.noreply.github.com>
2018-03-30 21:14:27 -0700

    Update etc.java

--- src/org/usfirst/frc/team4255/robot/etc.java
+++ src/org/usfirst/frc/team4255/robot/etc.java
@@ -5,14 +5,70 @@ public class etc {
 		{2.0, 0.0},
 		{0.0, -45.0},
 		{6.25, 0.0},
-		{0, 45.0},
+		{0.0, 45.0},
 	};
 	
 	public static double[][] middleR = {
-		{2.42, 0.0},
+		{2.42, 0.0}, //raise lift
 		{0.0, 45.0},
 		{5.07, 0.0},
-		{0, -45.0},
+		{0.0, -45.0}, //sensor and dropoff
+	};
+	public static double[][] leftL = {
+		{12.38, 0.0}, //raise lift
+		{0.0, 90.0} //dropoff switch
+	};
+	public static double[][] leftR = {
+		{17.44, 0.0}, //raise lift
+		{0.0, 90.0},
+		{13.72, 0.0},
+		{0.0, 90.0} //dropoff switch
+	};
+	public static double[][] rightR = {
+		{12.38, 0.0}, //raise lift
+		{0.0, -90.0} //dropoff switch
+	};
+	public static double[][] rightL = {
+		{17.44, 0.0}, //raise lift
+		{0.0, -90.0},
+		{13.72, 0.0},
+		{0.0, -90.0} //dropoff switch
+	};
+	public static double[][] leftLL = {
+		{12.38, 0.0}, //raise lift
+		{0.0, 90.0},
+		{1.59, 0.0}, //dropoff switch
+		{-1.0, 0.0},
+		{0.0, -90.0},
+		{6.36, 0.0},
+		{0.0, 90.0},
+		{3.17, 0.0},
+		{0.0, 90.0},
+		{1.58, 0.0}, //with intake activated -> pickup cube
+		{-1.0, 0.0},
+		{0.0, 90.0},
+		{3.79, 0.0},
+		{0.0, 90.0},
+		{0.5, 0.0}, //dropoff scale
+		{-1.66, 0.0}
+	};
+	public static double[][] leftLR = {
+		{12.38, 0.0}, //raise lift
+		{0.0, 90.0},
+		{1.59, 0.0}, //dropoff switch
+		{-1.0, 0.0},
+		{0.0, -90.0},
+		{6.36, 0.0},
+		{0.0, 90.0},
+		{3.17, 0.0},
+		{0.0, 90.0},
+		{1.58, 0.0}, //with intake activated -> pickup cube
+		{-1.0, 0.0},
+		{0.0, -90.0},
+		{11.82, 0.0},
+		{0.0, -90.0},
+		{6.81, 0.0}, //dropoff scale
+		{-1.66, 0.0}
 	};
 	
 	public static double map(double val, double fromMin, double fromMax, double toMin, double toMax) {
