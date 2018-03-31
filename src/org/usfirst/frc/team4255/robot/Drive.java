b0be5624222024e67839de00655396f21b329c23
ROBODORES <35854495+ROBODORES@users.noreply.github.com>
2018-03-30 21:18:21 -0700

    Update Drive.java

--- src/org/usfirst/frc/team4255/robot/Drive.java
+++ src/org/usfirst/frc/team4255/robot/Drive.java
@@ -1,43 +1,103 @@
 package org.usfirst.frc.team4255.robot;
 
-public class Route {
-	private double[][] ROUTE_ARRAY;
-	private Drive drive;
-	private NavDrive navDrive;
-	public int step = 0;
-	public int typeStep = 0;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+
+public class Drive {
+	public static TalonSRX leftDrive;
+	public static TalonSRX rightDrive;
+	private static long leftZero, rightZero;
+	private double driveSpeed;
 	
-	public Route(double[][] ROUTE_ARRAY, Drive drive, NavDrive navDrive) {
-		this.ROUTE_ARRAY = ROUTE_ARRAY;
-		this.drive = drive;
-		this.navDrive = navDrive;
-		step = 0;
-		typeStep = 0;
-		this.drive.reset();
-		this.navDrive.reset();
+	public Drive(TalonSRX leftDrive, FeedbackDevice leftFeedback, TalonSRX rightDrive, FeedbackDevice rightFeedback) {
+		this.leftDrive = leftDrive;
+		this.rightDrive = rightDrive;
+		leftZero = 0;
+		rightZero = 0;
+		driveSpeed = 0;
+		leftDrive.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 1, 10);
+		leftDrive.configSelectedFeedbackSensor(leftFeedback, 0, 10);
+		rightDrive.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 1, 10);
+		rightDrive.configSelectedFeedbackSensor(rightFeedback, 0, 10);
 	}
 	
-	public void setTo(int step) {
-		this.step = step;
-		drive.reset();
-		navDrive.reset();
-	}
-	
-	public boolean run() {
-		if (step < ROUTE_ARRAY.length) {
-			if (typeStep == 0) {
-				if(drive.driveTo(ROUTE_ARRAY[step][typeStep]) || ROUTE_ARRAY[step][typeStep] == 0) typeStep++;
-			} else {
-				if(navDrive.turnTo(ROUTE_ARRAY[step][typeStep]) || ROUTE_ARRAY[step][typeStep] == 0) {
-					typeStep = 0;
-					step++;
-					drive.reset();
-					navDrive.reset();
-				}
-			}
-			return false;
+	public void setDrive(double leftSpeed, double rightSpeed, boolean reverse) {
+		if (reverse) {
+			rightSpeed = -rightSpeed;
+			leftSpeed = -leftSpeed;
 		}
-		drive.setDrive(0.0, 0.0, false);
-		return true;
+		
+		leftDrive.set(ControlMode.PercentOutput, leftSpeed);
+		rightDrive.set(ControlMode.PercentOutput, rightSpeed);
+	}
+	
+	public static void singleJoystickDrive(double yVal, double xVal, boolean reverse) {
+		double leftSpeed = yVal-xVal;
+		if (leftSpeed > 1.0) leftSpeed = 1.0; if (leftSpeed < -1.0) leftSpeed = -1.0;
+		double rightSpeed = yVal+xVal;
+		if (leftSpeed > 1.0) leftSpeed = 1.0; if (leftSpeed < -1.0) leftSpeed = -1.0;
+		
+		if (reverse) {
+			rightSpeed = -rightSpeed;
+			leftSpeed = -leftSpeed;
+		}
+		
+		leftDrive.set(ControlMode.PercentOutput, leftSpeed);
+		rightDrive.set(ControlMode.PercentOutput, rightSpeed);
+	}
+	
+	public void zeroLeftDist() {leftZero = -leftDrive.getSelectedSensorPosition(0);}
+	
+	public static void zeroRightDist() {rightZero = rightDrive.getSelectedSensorPosition(0);}
+	
+	public static double leftDist() { //returns traveled distance in feet
+		return (double)(-leftDrive.getSelectedSensorPosition(0)-leftZero)/4096.0*(6*Math.PI)/12.0;
+	}
+	
+	public static double rightDist() { //returns traveled distance in feet
+		return (double)(rightDrive.getSelectedSensorPosition(0)-rightZero)/4096.0*(6*Math.PI)/12.0;
+	}
+	
+	public static double leftVelocity() { //returns velocity in feet/second
+		return ((double)(-leftDrive.getSelectedSensorVelocity(0)*10)/4096.0)*(6*Math.PI)/12.0;
+	}
+	
+	public static double rightVelocity() { //returns velocity in feet/second
+		return ((double)(-rightDrive.getSelectedSensorVelocity(0)*10)/4096.0)*(6*Math.PI)/12.0;
+	}
+	
+	void reset() {
+		zeroLeftDist();
+		zeroRightDist();
+		driveSpeed = 0.0;
+	}
+	
+	public boolean driveTo(double distance) { //don't run yet
+		setDrive(driveSpeed, driveSpeed, false);
+		double MAINTAINED_VELOCITY = 3.0; //ftps
+		double DECELERATION_BUFFER = 1.5; //ft
+		
+		if (Math.abs(rightDist()) >= Math.abs(distance)- DECELERATION_BUFFER) {
+			driveSpeed += (((MAINTAINED_VELOCITY/DECELERATION_BUFFER)*((distance-rightDist())/MAINTAINED_VELOCITY)) - rightVelocity())/500.0;
+			if (Math.abs(rightDist()) >= Math.abs(distance)) {
+				setDrive(0.0, 0.0, false);
+				return true;
+			}
+		}
+		else {
+			driveSpeed += (MAINTAINED_VELOCITY*(distance/Math.abs(distance)) - rightVelocity())/200.0;
+		}
+		return false;
+	}
+	
+	public boolean simpleDriveTo(double speed, double distance) {
+		setDrive(Math.abs(speed)*(distance/Math.abs(distance)), Math.abs(speed)*(distance/Math.abs(distance)), false);
+		if (Math.abs(rightDist()) >= Math.abs(distance)) {
+			setDrive(0.0, 0.0, false);
+			return true;
+		}
+		return false;
 	}
 }
